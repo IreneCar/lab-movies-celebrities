c8a8aceebb47af8a6e4b5b3558e9b1e7b1664644
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-06-28 15:41:05 +0200

    connecting model to celebrities route

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,9 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+//requerir modelo
+const Celebrity = require("../models/Celebrity.model")
+
 //iteration #3 - Adding new Celebrities
 router.get('/celebrities/create', (req, res, next) => {
   res.render('celebrities/new-celebrity.hbs');
